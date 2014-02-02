0f3c466975878e83b441a60c758f56912118a979
craynerd <chris@raynerd.co.uk>
2014-02-02 00:31:43 +0000

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -11,10 +11,17 @@ ACCESS_SECRET = 'XXXXXXXXXX'
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
-def send_tweet(temp_value, temp_symbol, light_value, light_symbol, pressure_value, pressure_symbol, volume_value, volume_symbol):
-        try: 
-                photo = open('twitterpicture.jpg','rb')
+def send_tweet1(temp_value, temp_symbol, light_value, light_symbol, pressure_value, pressure_symbol, volume_value, volume_symbol):
+        try:
+	        photo = open('twitterpicture.jpg','rb')
 		api.update_status_with_media(media=photo, status="Temp: {0}{1}. Light Level: {2:.2f} {3}. Pressure: {4:.2f} {5}. Volume: {6:.2f} {7}.".format(temp_value, temp_symbol, light_value, light_symbol, pressure_value, pressure_symbol, volume_value, volume_symbol))
+                print "Successfully posted tweet."
+        except TwythonError:
+                print "Failed to post tweet - duplicate"
+
+def send_tweet2(temp_value, temp_symbol, light_value, light_symbol, pressure_value, pressure_symbol, volume_value, volume_symbol):
+        try:
+		api.update_status(status="Temp: {0}{1}. Light Level: {2:.2f} {3}. Pressure: {4:.2f} {5}. Volume: {6:.2f} {7}.".format(temp_value, temp_symbol, light_value, light_symbol, pressure_value, pressure_symbol, volume_value, volume_symbol))
                 print "Successfully posted tweet."
         except TwythonError:
                 print "Failed to post tweet - duplicate"
